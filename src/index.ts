d4c616247c6d29a9890832a346abb1699007e76e
mohdthoriq <mohdthoriq@gmail.com>
2026-04-06 09:36:22 +0700

    done : set up BE

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import app from "./app";
-import config from './utils/env'
-
-app.listen(config.PORT, () => {
-    console.log(`Server running at → ${config.HOST}`);
-    console.log(`Coba buka semua route di atas pakai Postman!`);
-});
